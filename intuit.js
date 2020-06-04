e937d2c6cbf9e3a381ea54f73c92509b94f9902a
Nicoletta <nicoletta.hirsch@gmail.com>
2020-06-04 13:33:13 +0200

    asdf

---
+++ intuit.js
@@ -0,0 +1,51 @@
+//input
+
+const showInfo =  (targetId, infoId) => {
+
+  document.addEventListener("click", (e) => {
+      
+    
+
+  })
+}
+
+//vereinfachen!!! module pattern???? create an object ? 
+
+if (e.target.id === "phone") {
+    const info = document.getElementById("info-phone");
+else if (e.target.id === "email") {
+    const info = document.getElementById("info-email");
+}
+else if(e.target.id === "password") {
+    const info = document.getElementById("info-password");
+}
+}
+
+//email
+
+
+
+//phone
+
+
+
+
+//password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// placeholder ="You'll verify this later"
+// placeholder="Helps with lost passwords and security"
+// placeholder = "Don't reuse an old password" 
