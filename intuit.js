507628f7bd08fa0a2091db76a9951f734f5c151d
Nico Nacht <nd.hagedorn@gmail.com>
2020-06-04 14:42:55 +0200

    asdf

--- intuit.js
+++ intuit.js
@@ -1,39 +1,49 @@
 
-//INPUT
 
 
-document.addEventListener("click", (e) => {
-    const inputs = Array.from(document.getElementsByClassName("input"));
-    for (let i = 0; i < inputs.length; i++) {
-       if (e.target === inputs[i]){
-           const id = e.target.id;
-           console.log(id);
-           const outputId = `info-${id}`;
-           const outputId = "info-phone";
-           console.log(outputId);
-           document.getElementById("info-phone").classList.remove("hidden");
-       }   
-   }
-});
+const showInfo = () => {
+    document.addEventListener("click", (e) => {
+        const inputs = Array.from(document.getElementsByClassName("input"));
+        for (let i = 0; i < inputs.length; i++) {
+            const id = inputs[i].id;
+            const outputId = `info-${id}`;
+            const infoText = document.getElementById(outputId);  
+          
+            if (e.target === inputs[i]){
+               infoText.classList.remove("hidden");
+           } 
+           else {
+               if
+               (!infoText.classList.contains("hidden")) {
+                   infoText.classList.add("hidden");
+               };
+           }  
+       }
+    });
+}
 
-//EMAIL
+const addEmail = () =>  {
+ 
+
+const addPhone = (e) => {
+    if (e.target.id ==="phone"){
+        
+        
+    }
+
+const addPassword = (e) =>  {
+
+}
 
 
 
-//PHONE
-
-
-
-//PASSWORD
 
 
 
 
 
 
-
-
-
+showInfo();
 // placeholder ="You'll verify this later"
 // placeholder="Helps with lost passwords and security"
 // placeholder = "Don't reuse an old password" 
