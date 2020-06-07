ed096e8c615d779aad85d2ad85165bb200377534
Nicoletta <nicoletta.hirsch@gmail.com>
2020-06-07 14:42:37 +0200

    asdf

--- intuit.js
+++ intuit.js
@@ -1,39 +1,64 @@
 
-//INPUT
 
 
-document.addEventListener("click", (e) => {
-    const inputs = Array.from(document.getElementsByClassName("input"));
-    for (let i = 0; i < inputs.length; i++) {
-       if (e.target === inputs[i]){
-           const id = e.target.id;
-           console.log(id);
-           const outputId = `info-${id}`;
-           const outputId = "info-phone";
-           console.log(outputId);
-           document.getElementById("info-phone").classList.remove("hidden");
-       }   
-   }
-});
+const showInfo = () => {
+    document.addEventListener("click", (e) => {
+        const inputs = Array.from(document.getElementsByClassName("input"));
+        for (let i = 0; i < inputs.length; i++) {
+            const id = inputs[i].id;
+            const outputId = `info-${id}`;
+            const infoText = document.getElementById(outputId);  
+          
+            if (e.target === inputs[i]){
+               infoText.classList.remove("hidden");
+               addPhone(e.target.id);
+           } 
+           else {
+               if
+               (!infoText.classList.contains("hidden")) {
+                   infoText.classList.add("hidden");
+               };
+           }  
+       }
+    });
+}
 
-//EMAIL
+const addEmail = () =>  {}
+ 
+
+const addPhone = (target) => {
+    const html = 
+    `
+    <div id="fieldset-verify">
+    <input id="verify" type="checkbox" name="verify" checked>
+        <span id="verify-text" >Verify with text message </span>
+        
+        <span class="very-small" id="verify-italic">(most secure)</span>
+    </div>
+    `
+    const isVerified = document.getElementById("fieldset-verify")
+    if (target ==="phone" && !isVerified){
+      const phoneField = document.getElementById("fieldset-phone");
+      phoneField.innerHTML += html;
+
+        
+        
+    }
+}
+
+const addPassword = (e) =>  {
+
+}
 
 
 
-//PHONE
-
-
-
-//PASSWORD
 
 
 
 
 
 
-
-
-
+showInfo();
 // placeholder ="You'll verify this later"
 // placeholder="Helps with lost passwords and security"
 // placeholder = "Don't reuse an old password" 
